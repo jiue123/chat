968ddaa26a8090a9edd1041a8edb4f4e8e61b35a
PhatLe <lethanhphat0208@gmail.com>
2018-08-06 17:35:51 +0700

    feature/#1_make_form_chat push code first time

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/chat';
 
     /**
      * Create a new controller instance.
